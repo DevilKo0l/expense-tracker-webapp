305620bdc2ce6a49b1bc8c674d6642b3fe565f37
DevilKo0l <nguyen.tcao96@gmail.com>
2022-06-20 23:44:44 +0200

    Update Transaction List

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,11 +6,11 @@ import {
   Typography,
   Grid,
   Divider,
-  List,
 } from "@material-ui/core";
 
 import useStyles from "./styles";
 import Form from "./Form/Form";
+import List from "./List/List";
 const Main = () => {
   const classes = useStyles();
   return (
@@ -32,7 +32,7 @@ const Main = () => {
       <CardContent className={classes.cartContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List/> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
